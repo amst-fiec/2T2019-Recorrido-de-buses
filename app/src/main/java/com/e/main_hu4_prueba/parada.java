288fff1d8c8a3479a4342c72cb76d23db0fdaf2b
ibaquerizo <54192631+ibaquerizo@users.noreply.github.com>
2020-01-24 23:40:54 -0500

    Paradas
    Registra paradas en la base de datos

--- app/src/main/java/com/e/main_hu4_prueba/parada.java
+++ app/src/main/java/com/e/main_hu4_prueba/parada.java
@@ -1,21 +1,22 @@
 package com.e.main_hu4_prueba;
 
 public class parada {
-    public String nombre_parada, coordenadas;
+    public String nombre_parada, coordenadas1,coordenadas2;
     public float latitud, longitud;
 
-    public parada(String nombre_parada, String coordenadas){
-        this.coordenadas=coordenadas;
+    public parada(String nombre_parada, String coordenadas1, String coordenadas2){
+        this.coordenadas1=coordenadas1;
+        this.coordenadas2=coordenadas2;
         this.nombre_parada=nombre_parada;
     }
 
     public float latitud_f(){
-        latitud= Float.parseFloat(coordenadas.split(";")[0]);
+        latitud= Float.parseFloat(coordenadas1);
         return latitud;
     }
 
     public float longitud_f(){
-        latitud= Float.parseFloat(coordenadas.split(";")[1]);
+        latitud= Float.parseFloat(coordenadas2);
         return longitud;
     }
 
@@ -27,11 +28,23 @@ public class parada {
         this.nombre_parada = nombre_parada;
     }
 
-    public String getCoordenadas() {
-        return coordenadas;
+    public String getCoordenadas1() {
+        return coordenadas1;
     }
 
-    public void setCoordenadas(String coordenadas) {
-        this.coordenadas = coordenadas;
+    public void setCoordenadas1(String coordenadas1) {
+        this.coordenadas2 = coordenadas1;
     }
+
+    public String getCoordenadas2() {
+        return coordenadas2;
+    }
+
+    public void setCoordenadas2(String coordenadas2) {
+        this.coordenadas2 = coordenadas2;
+    }
+
+
+
 }
+
--- app/src/main/java/com/e/main_hu4_prueba/registrar_parada_conductor.java
+++ app/src/main/java/com/e/main_hu4_prueba/registrar_parada_conductor.java
@@ -1,19 +1,31 @@
 package com.e.main_hu4_prueba;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class registrar_parada_conductor extends AppCompatActivity {
 
     Button btn_reg;
-    TextView txt_parda, txt_coordenadas;
+    TextView txt_parda, txt_coordenadas1,txt_coordenadas2;
     parada parada_o;
     String parada_fromTXT, coor_fromTXT, nombre_parada;
     float latitud,longitud;
+    DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +33,8 @@ public class registrar_parada_conductor extends AppCompatActivity {
         setContentView(R.layout.activity_registrar_parada);
 
         btn_reg= (Button) findViewById(R.id.btn_registrar);
-        txt_coordenadas=(TextView) findViewById(R.id.edit_coordenadas);
+        txt_coordenadas1=(TextView) findViewById(R.id.edit_coordenadas1);
+        txt_coordenadas2=(TextView) findViewById(R.id.edit_coordenadas1);
         txt_parda= (TextView) findViewById(R.id.edit_nombre_parada);
 
         btn_reg.setOnClickListener(new View.OnClickListener() {
@@ -34,17 +47,38 @@ public class registrar_parada_conductor extends AppCompatActivity {
                 //parada_fromTXT=txt_parda.getEditableText().toString();
 
                 //creo objeto
+
                 parada_o= new parada (txt_parda.getEditableText().toString(),
-                        txt_coordenadas.getEditableText().toString()
+                        txt_coordenadas1.getEditableText().toString(),txt_coordenadas2.getEditableText().toString()
                 );
 
                 //adquiero parametros; longitud, latitud y nombre parada
                 longitud=parada_o.longitud_f();
                 latitud=parada_o.latitud_f();
                 nombre_parada=parada_o.getNombre_parada();
+                Map<String, Object> mapP = new HashMap<>();
+                mapP.put("longitud", parada_o.getCoordenadas2());
+                mapP.put("latitud", parada_o.getCoordenadas1());
+                mapP.put("nombre parada", nombre_parada);
+                mDatabase = FirebaseDatabase.getInstance().getReference();
+
+                mDatabase.child("Paradas").push().setValue(mapP).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task2) {
+                        if (task2.isSuccessful()) {
+                            Toast.makeText(registrar_parada_conductor.this, "Se ha registrado la parada", Toast.LENGTH_SHORT).show();
+
+
+                        } else {
+                            Toast.makeText(registrar_parada_conductor.this, "No se pudo crear los datos correctamente", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
 
                 System.out.print("longitud, latitud: ");
                 System.out.println(latitud);
+                startActivity(new Intent(registrar_parada_conductor.this, panel_opcion_conductor.class));
 
 
             }
