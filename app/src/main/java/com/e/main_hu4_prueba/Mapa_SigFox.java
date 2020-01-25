615223160d642245ffb96f8755f765b41e80be41
ibaquerizo <54192631+ibaquerizo@users.noreply.github.com>
2020-01-24 19:24:59 -0500

    MapaActualizado

--- app/src/main/java/com/e/main_hu4_prueba/Mapa_SigFox.java
+++ app/src/main/java/com/e/main_hu4_prueba/Mapa_SigFox.java
@@ -12,14 +12,24 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Mapa_SigFox extends FragmentActivity implements OnMapReadyCallback {
     private Button btncerrar;
     private GoogleMap mMap;
     private FirebaseAuth mAuth;
+    public DatabaseReference db_reference;
+    public double latVal;
+    public double longVal;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,12 +64,52 @@ public class Mapa_SigFox extends FragmentActivity implements OnMapReadyCallback 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.clear();
         UiSettings uiSettings= mMap.getUiSettings();
         uiSettings.setZoomControlsEnabled(true);
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-2.1961601, -79.8862076);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setMyLocationEnabled(true);
+        latVal=-2.1629;
+        longVal=-79.9389;
+        db_reference = FirebaseDatabase.getInstance().getReference().child("Dispotivo");
+
+        db_reference.addValueEventListener(new ValueEventListener() { @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                String latVal1 = String.valueOf(snapshot.child("Latitud").getValue());
+                String longVal1 = String.valueOf(snapshot.child("Longitud").getValue());
+
+                if (latVal1.equals("null") || longVal1.equals("null")){
+                    latVal=-2.1629;
+                    longVal=-79.9389;
+                }
+                else if (latVal1.equals(" ") || longVal1.equals(" ")){
+                    latVal=-2.1629;
+                    longVal=-79.9389;
+                }
+                else{
+                    latVal= new Double(latVal1);
+                    longVal=new Double (longVal1);
+
+                } }}
+            @Override
+            public void onCancelled(DatabaseError error) { System.out.println(error.toException());
+            } });
+
+
+        LatLng ubicacion = new LatLng(latVal,longVal);
+        CameraPosition cameraPosition = CameraPosition.builder().target(ubicacion)
+                .zoom(18)
+                .bearing(0)
+                .tilt(45)
+                .build();
+
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        mMap.addMarker(new MarkerOptions().position(ubicacion).title("Marker"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ubicacion));
+
+
     }
 }
